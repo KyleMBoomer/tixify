39b98c8b1e130fdfd7f648b1b13819109fd2f9d3
Kyle Mark Boomer <kyle.boomer@smartwayslfl.com>
2026-05-08 13:40:57 -0500

    fix prisma file

--- nextjs-prisma/lib/prisma.ts
+++ nextjs-prisma/lib/prisma.ts
@@ -1,12 +1,8 @@
 import { PrismaClient } from '@/app/generated/prisma';
 
 declare global {
-  // allow global `var` declarations
-  // eslint-disable-next-line no-var, vars-on-top
-  var PRISMA: PrismaClient | null;
-};
+  var prisma: PrismaClient | undefined;
+}
 
-export const PRISMA = global.PRISMA ?? new PrismaClient()
-if (process.env.NODE_ENV !== 'production') {
-    global.PRISMA = PRISMA
-    }
+export const prisma = global.prisma ?? new PrismaClient();
+if (process.env.NODE_ENV !== 'production') global.prisma = prisma;
