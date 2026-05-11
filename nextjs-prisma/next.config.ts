1b7153d38a16ab01bef9b448f8036545f88a7c92
Kyle Mark Boomer <kyle.boomer@smartwayslfl.com>
2026-05-11 14:33:31 -0500

    feat: updated config for loopback IP

--- nextjs-prisma/next.config.ts
+++ nextjs-prisma/next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  turbopack: {
+    root: __dirname,
+  },
+  allowedDevOrigins: ['http://127.0.0.1:3000'],
+  env: {
+    AUTH_URL: 'http://127.0.0.1:3000',
+    NEXTAUTH_URL: 'http://127.0.0.1:3000',
+  },
 };
 
 export default nextConfig;
