a2fa1f78d0247ef823a3a02904c0e3bcd87bc9d1
Kyle M. Boomer <kyle.boomer@smartwayslfl.com>
2026-05-12 09:49:10 -0500

    feat: added scope for playback to auth

--- nextjs-prisma/auth.ts
+++ nextjs-prisma/auth.ts
@@ -32,7 +32,7 @@ export const authConfig: NextAuthConfig = {
     Spotify({
       clientId: process.env.SPOTIFY_CLIENT_ID!,
       clientSecret: process.env.SPOTIFY_CLIENT_SECRET!,
-      authorization: 'https://accounts.spotify.com/authorize?scope=user-top-read+user-read-email+user-read-private',
+      authorization: 'https://accounts.spotify.com/authorize?scope=user-top-read+user-read-email+user-read-private+streaming+user-read-playback-state+user-modify-playback-state',
     }),
   ],
   callbacks: {
